b04552c34edb253958eb41e06ee02093c79272ab
Mugund <31250018+Mugund-K@users.noreply.github.com>
2024-06-28 11:57:40 +0530

    Updated hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,23 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Mugund</h2>;
+  const three = <h3 className="big-heading">I build AI systems and research quantum computing advancements.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
+      I'm an ML Engineer specializing in developing (and occasionally optimizing) advanced artificial intelligence systems. Currently, I'm focused on building machine learning solutions at TalentNeuron while pursuing my PhD research on optimizing Large Language Model training using quantum computing techniques.
         .
       </p>
     </>
   );
   const five = (
     <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    className="email-link"
+    href="mailto:hello@mugund.in"
+    target="_blank"
+    rel="noreferrer">
+    Contact me!
     </a>
   );
 
