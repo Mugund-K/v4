5bcbd67a663c5d15c56860d6c99f74fc51769091
neuralzeal <neuralzealn@gmail.com>
2025-03-18 00:09:23 +0530

    Your commit message

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@
 
 const path = require('path');
 const _ = require('lodash');
-
+const stream = require('stream');
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
   const postTemplate = path.resolve(`src/templates/post.js`);
