65b30300ac475629a15de1d53add1c2193fb4185
neuralzeal <neuralzealn@gmail.com>
2025-03-18 23:32:38 +0530

    update to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Mugund K',
+    title: 'Brittany Chiang',
     description:
-      'Mugund is an ML Engineer specializing in developing (and occasionally optimizing) advanced artificial intelligence systems. Currently focused on building machine learning solutions at TalentNeuron while pursuing my PhD research on optimizing Large Language Model training using quantum computing techniques',
-    siteUrl: 'https://mugund.in', // No trailing slash allowed!
+      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@k.mugund',
+    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Mugund K',
-        short_name: 'Mugund K',
+        name: 'Brittany Chiang',
+        short_name: 'Brittany Chiang',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
