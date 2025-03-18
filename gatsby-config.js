b0da280ecb8f43a7e532505c2d77dad06765c3a5
neuralzeal <neuralzealn@gmail.com>
2025-03-18 23:41:47 +0530

    removed yarn lock file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Mugund',
+    description: 'Mugund build AI systems and research quantum computing advancements.',
+    siteUrl: 'https://mugund.in', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@k.mugund',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
